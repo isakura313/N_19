c35a69036ca464d5882e15f8febe837603c75973
Paul <isakura313@gmail.com>
2020-05-25 17:35:19 +0300

    asda

---
+++ second_lesson/interface.py
@@ -0,0 +1,7 @@
+from kinder import kid_neuro
+
+epoch = int(input("Эпохи: \n"))
+lr = float(input("Введите скорость обучения: \n"))
+accur = float(input("Введите допустимую точность от 0.1 до 0.05: \n"))
+
+kid_neuro(epoch, lr, accur)
